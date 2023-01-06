1a933b31a376bff2affe993664afcd779914fa37
Joseph Bergmann <joseph.g.bergmann@gmail.com>
2023-01-06 14:42:22 -0500

    did bind

--- ejerciciosargumentos.js
+++ ejerciciosargumentos.js
@@ -1,22 +1,50 @@
-function sum(){
-    let sum = 0;
-    Array.from(arguments).forEach(element => {
-        sum += element;
-    });
-    return sum
+Function.prototype.myBind = function(context, ...bindArgs){
+    return (...callArgs) => {
+        return this.apply(context, bindArgs.concat(callArgs));
+    };
 }
 
-console.log(sum(1, 2, 3, 4) === 10);
-console.log(sum(1, 2, 3, 4, 5) === 15);
-
-function goodSum(...args){
-    let sum = 0;
-    args.forEach(element => {
-        sum += element;
-    });
-    return sum;
-}
-
-console.log(goodSum(1, 2, 3, 4) === 10); //
-console.log(goodSum(1, 2, 3, 4, 5) === 15);
-
+class Cat {
+    constructor(name) {
+      this.name = name;
+    }
+  
+    says(sound, person) {
+      console.log(`${this.name} says ${sound} to ${person}!`);
+      return true;
+    }
+  }
+  
+  class Dog {
+    constructor(name) {
+      this.name = name;
+    }
+  }
+  
+  const markov = new Cat("Markov");
+  const pavlov = new Dog("Pavlov");
+  
+  markov.says("meow", "Ned");
+  // Markov says meow to Ned!
+  // true
+  
+  // bind time args are "meow" and "Kush", no call time args
+  markov.says.myBind(pavlov, "meow", "Kush")();
+  // Pavlov says meow to Kush!
+  // true
+  
+  // no bind time args (other than context), call time args are "meow" and "a tree"
+  markov.says.myBind(pavlov)("meow", "a tree");
+  // Pavlov says meow to a tree!
+  // true
+  
+  // bind time arg is "meow", call time arg is "Markov"
+  markov.says.myBind(pavlov, "meow")("Markov");
+  // Pavlov says meow to Markov!
+  // true
+  
+  // no bind time args (other than context), call time args are "meow" and "me"
+  const notMarkovSays = markov.says.myBind(pavlov);
+  notMarkovSays("meow", "me");
+  // Pavlov says meow to me!
+  // true
---
+++ sum.js
@@ -0,0 +1,19 @@
+function sum() {
+    let sum = 0;
+    Array.from(arguments).forEach(element => {
+        sum += element;
+    });
+    return sum;
+}
+console.log(sum(1, 2, 3, 4) === 10);
+console.log(sum(1, 2, 3, 4, 5) === 15);
+function goodSum(...args) {
+    let sum = 0;
+    args.forEach(element => {
+        sum += element;
+    });
+    return sum;
+}
+console.log(goodSum(1, 2, 3, 4) === 10); //
+
+console.log(goodSum(1, 2, 3, 4, 5) === 15);
